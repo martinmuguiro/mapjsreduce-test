48cd0d615163c37e18b0edd1ce3238f560b4723e
Juanan Pereira <juanan.pereira@ehu.eus>
2023-09-25 14:59:05 +0200

    fix test9

--- tests/chaining.test.js
+++ tests/chaining.test.js
@@ -13,8 +13,7 @@ test('ariketa9 filters users older than or equal to 18 and calculates the total 
         { izena: "Pepe", adina: 18, erosketak: [25,12,30] },
         { izena: "Ane", adina: 19, erosketak: [1.75,4.60,3.00] }
     ];
-    expect(ariketa9(users)).toEqual([{ izena: "Pepe", erosketak: 67 }, { izena: "Ane", erosketak: 9.35 }]);
-});
+    expect(ariketa9(users)).toEqual(76.35);
 
 
 test('ariketa10 calculates the average score for each student and finds the one with the highest average', () => {
