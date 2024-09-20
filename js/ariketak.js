dad25de774ddf9750573edb6a0710cbb67ba1b57
Martin Muguiro Eseverri <123961022+martinmuguiro@users.noreply.github.com>
2024-09-20 11:10:23 +0000

    ariketak

--- js/ariketak.js
+++ js/ariketak.js
@@ -1,4 +1,25 @@
 // idatzi hemen zure funtzioak
+function ariketa1( arrays ){
+    return arrays.map(e => e *2);
+}
+
+function ariketa2(){}
+
+function ariketa3(){}
+
+function ariketa4(){}
+
+function ariketa5(){}
+
+function ariketa6(){}
+
+function ariketa7(){}
+
+function ariketa8(){}
+
+function ariketa9(){}
+
+function ariketa10(){}
 
 
 export {ariketa1, ariketa2, ariketa3, ariketa4, ariketa5, ariketa6, ariketa7, ariketa8, ariketa9, ariketa10}
